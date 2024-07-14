33f99fdb8e3bd9d1e4abfeb673a0f078e6af6ffb
Aniket-paul-alt <113275960+Aniket-paul-alt@users.noreply.github.com>
2024-07-14 14:29:23 +0530

    first commit

---
+++ 01-Variable & DataTypes/script.js
@@ -0,0 +1,37 @@
+//Activity 1 - variable declaration
+//task-1
+var number = 10
+console.log(number);
+//task-2
+let string = `Aniket`
+console.log(string);
+
+//Activity 2 - constant declaration
+//task-3
+const boolean = true;
+console.log(boolean);
+
+//Activity 3 - Data Types
+//task-4
+let num = 1
+let str = `tukai`
+let boo = false
+let obj = {name: `Paul`, age: 22}
+let arr = [ `Ani`, 15, true, {play: `Football`, rank: 1}]
+
+console.table([typeof num, typeof str, typeof boo, typeof obj, typeof arr])
+console.table([ num, str, boo, obj, arr])
+
+//Activity 4 - reassigning variables
+//task 5
+let assign = 10
+console.log(assign);
+assign = 12
+console.log(assign);
+
+//Activity 5 - understanding const
+//task 6
+const declare = 12
+console.log(declare);
+declare = 13 // cant possible
+console.log(declare); // error : Assignment to constant variable.
